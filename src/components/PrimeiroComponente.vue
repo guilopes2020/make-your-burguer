99056ea6f649554f66359ea9746aff9347788307
guilopes2020 <guilopesdev@gmail.com>
2024-08-15 16:46:00 -0300

    feat: :sparkles: creating-first-componnents

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Olá Vue!</h1>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: blue;
+    }
+</style>
