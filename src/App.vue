78b1315a15934788c64250220ada7e8598d51ba1
guilopes2020 <guilopesdev@gmail.com>
2024-08-15 17:21:15 -0300

    feat: :sparkles: understanding-lifecycle-hooks

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <PrimeiroComponente />
+  <LifeCycle />
 </template>
 
 <script>
+  import LifeCycle from './components/LifeCycle.vue';
   import PrimeiroComponente from './components/PrimeiroComponente.vue' 
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     }
   }
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'ainda nao sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Matheus'
+            }, 1000)
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Pedro'
+            }, 2000)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
