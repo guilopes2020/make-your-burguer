c2670ce02101e7cf66553e2cf06aecfb173f84b6
guilopes2020 <guilopesdev@gmail.com>
2024-08-15 16:59:14 -0300

    feat: :sparkles: inserting-data-into-components

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,15 +1,29 @@
 <template>
     <h1>Olá Vue!</h1>
+    <p>Meu nome é: <span>{{ nome }}</span>, sou {{ profissao }} e tenho {{ idade }} anos!</p>
 </template>
 
 <script>
     export default {
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: 'Guilherme',
+                profissao: 'programador',
+                idade: 42
+            }
+        }
     }
 </script>
 
 <style scoped>
     h1 {
         color: blue;
+    }
+    p {
+        color: black;
+    }
+    span {
+        color: crimson;
     }
 </style>
