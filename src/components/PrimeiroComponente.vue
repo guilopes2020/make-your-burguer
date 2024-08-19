33c6af57e3fdc55dcfd3e08855eabbe349ae6172
guilopes2020 <guilopesdev@gmail.com>
2024-08-19 08:20:47 -0300

    feat: :sparkles: component-reuse

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,15 +1,22 @@
 <template>
+    <Reultilizacao />
     <h1>Olá Vue!</h1>
     <p>Meu nome é: <span>{{ nome }}</span>, sou {{ profissao }} e tenho {{ idade }} anos!</p>
     <MultiploEventos />
+    <Reultilizacao />
+    <Reultilizacao />
+    <Reultilizacao />
+    <Reultilizacao />
 </template>
 
 <script>
 import MultiploEventos from './MultiploEventos.vue'
+import Reultilizacao from './Reultilizacao.vue'
     export default {
         name: 'PrimeiroComponente',
         components: {
-            MultiploEventos
+            MultiploEventos,
+            Reultilizacao
         },
         data() {
             return {
